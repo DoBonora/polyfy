f34b5110d326d3151552181dec9e48900ff0866e
pehamTom <pehamTom@gmx.at>
2021-08-09 11:10:51 +0200

    lexicographic comparison

--- src/Term.cpp
+++ src/Term.cpp
@@ -27,23 +27,25 @@ Term operator*(const Term &t1, const Term &t2) {
 }
 
 bool Term::operator>(const Term &other) const {
-  auto it1 = variables.begin(), it2 = other.variables.begin();
-  for (; it1 != variables.end() && it2 != other.variables.end() && *it1 == *it2;
-       ++it1, ++it2)
-    ;
-  return (it1 != variables.end() && it2 == other.variables.end()) ||
-         (it1 != variables.end() && it2 != other.variables.end() &&
-          *it1 > *it2);
+  return std::lexicographical_compare(other.variables.begin(), other.variables.end(), variables.begin(), variables.end());
+  // auto it1 = variables.begin(), it2 = other.variables.begin();
+  // for (; it1 != variables.end() && it2 != other.variables.end() && *it1 == *it2;
+  //      ++it1, ++it2)
+  //   ;
+  // return (it1 != variables.end() && it2 == other.variables.end()) ||
+  //        (it1 != variables.end() && it2 != other.variables.end() &&
+  //         *it1 > *it2);
 }
 
 bool Term::operator>=(const Term &other) const {
-  auto it1 = variables.begin(), it2 = other.variables.begin();
-  for (; it1 != variables.end() && it2 != other.variables.end() && *it1 == *it2;
-       ++it1, ++it2)
-    ;
-  return (it2 == other.variables.end()) ||
-         (it1 != variables.end() && it2 != other.variables.end() &&
-          *it1 >= *it2);
+  return *this > other || *this == other;
+  // auto it1 = variables.begin(), it2 = other.variables.begin();
+  // for (; it1 != variables.end() && it2 != other.variables.end() && *it1 == *it2;
+  //      ++it1, ++it2)
+  //   ;
+  // return (it2 == other.variables.end()) ||
+  //        (it1 != variables.end() && it2 != other.variables.end() &&
+  //         *it1 >= *it2);
 }
 
 bool Term::operator<(const Term &other) const { return other > *this; }
